d844e574a376a49976565fc2bda9c3a2214a1e85
angeloCioce <angelocioce98@gmail.com>
2024-02-06 15:27:49 +0100

    clean the code

--- src/main/java/com/example/caffetteria/service/OrdineServiceImpl.java
+++ src/main/java/com/example/caffetteria/service/OrdineServiceImpl.java
@@ -8,9 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 
